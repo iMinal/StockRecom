69e10752ed7815e83954a776785fb3e66c8cae00
iMinal <minal.firodiya@gmail.com>
2018-03-16 17:39:00 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {StockData} from './stock-data';
 import {StockService} from './stock.service';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { NgZone } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,14 +13,16 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 export class AppComponent implements OnInit{
   title = 'app';
   dataset:any;
-id:any;
-  constructor(private stockService:StockService){}
+  id:any;
+  constructor(private stockService:StockService, private zone:NgZone){}
 
   private stockData:StockData[];
 
   ngOnInit(){
     this.stockService.getStock().subscribe((stockData)=>{
       console.log(stockData);
+      this.stockData = stockData;
+      console.log(this.stockData);
     },(error)=>{
       console.log(error);
     })
